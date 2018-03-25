4cf2cb51b31abf85e82570bba68374a9b68738fd
Yann Klis <yann.klis@gmail.com>
2018-03-25 11:03:21 +0200

    added rest-client request payload

--- lib/rest_client/request.rb
+++ lib/rest_client/request.rb
@@ -1,7 +1,7 @@
 module RestClient
   module RequestWithInstrumentation
     def execute(& block)
-      ActiveSupport::Notifications.instrument('log.restclient', url: url, method: method, headers: headers) do
+      ActiveSupport::Notifications.instrument('log.restclient', url: url, method: method, headers: headers, payload: payload.to_s) do
         super &block
       end
     end
--- lib/rest_client_logger/rest_client_log_subscriber.rb
+++ lib/rest_client_logger/rest_client_log_subscriber.rb
@@ -2,9 +2,10 @@ class RestClientLogSubscriber < ActiveSupport::LogSubscriber
   def log(event)
     return unless logger.debug?
     method = (event.payload[:method] || "").upcase
-    headers = event.payload[:headers] 
+    headers = event.payload[:headers]
+    payload = event.payload[:payload]
     name = 'RestClient %s (%.1fms)' % [method, event.duration]
-    debug "  #{color(name, GREEN, true)}  #{color(event.payload[:url], BOLD, true)}  #{headers.inspect if headers.present?}"
+    debug "  #{color(name, GREEN, true)}  #{color(event.payload[:url], BOLD, true)}  #{headers.inspect if headers.present?} #{payload.inspect if payload.present?}"
   end
 end
 
